d43f7b701f63cb3c1eebd31ff9049b353f030192
Peter Lisborg <159829271+Peter-Lisborg@users.noreply.github.com>
2025-10-06 17:58:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,36 @@
-const { ExpressPeerServer } = require('peer');
 const express = require('express');
+const { ExpressPeerServer } = require('peer');
 const cors = require('cors');
+const http = require('http');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://chattz.net', 'https://www.chattz.net'],
+  methods: ['GET', 'POST'],
+}));
 
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log('Listening...');
-});
-
+const server = http.createServer(app);
 const peerServer = ExpressPeerServer(server, {
-  debug: true,
-  path: '/peerjs'
+  debug: process.env.NODE_ENV !== 'production',
+  path: '/peerjs',
 });
 
 app.use('/chattz', peerServer);
 
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
+});
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
+process.on('SIGINT', () => {
+  console.log('Shutting down PeerJS server...');
+  server.close(() => process.exit(0));
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`PeerJS server running on port ${process.env.PORT || 3000}`);
 });
